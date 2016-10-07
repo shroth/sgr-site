91753b6c75b856493276a2a516b13f9167a4688f
Sharon Roth <SharonRoth@Sharons-MacBook-Pro.local>
2016-10-07 10:27:41 -0400

    Project Outline

--- send_form_email.php
+++ send_form_email.php
@@ -6,7 +6,7 @@ if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
  
-    $email_to = "email@email.com";
+    $email_to = "shroth14@gmail.com";
  
     $email_subject = "New message";
  
